09e92c0d96ba5b5ea6ef8d28c58b6a6c71a7b091
Jonathan Lehr <jonathan@aboutobjects.com>
2020-02-28 08:02:48 -0800

    Coolness2: Completed stage one implementation

---
+++ Coolness2/Coolness2/CLNCoolController.m
@@ -0,0 +1,88 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "CLNCoolController.h"
+#import "CLNCoolViewCell.h"
+
+@interface CLNCoolController () <UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UIView *contentView;
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@end
+
+@implementation CLNCoolController
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (IBAction)addCell {
+    NSLog(@"In %s", __func__);
+    CLNCoolViewCell *newCell = [[CLNCoolViewCell alloc] init];
+    newCell.text = self.textField.text;
+    newCell.backgroundColor = UIColor.systemBlueColor;
+    [self.contentView addSubview:newCell];
+}
+
+- (void)loadView1 {
+    [NSBundle.mainBundle loadNibNamed:@"CoolStuff" owner:self options:nil];
+}
+
+- (void)loadView2 {
+    NSArray *topLevelObjs = [NSBundle.mainBundle loadNibNamed:@"CoolStuff" owner:nil options:nil];
+    self.view = topLevelObjs.firstObject;
+}
+
+- (void)loadView_old {
+    self.view = [[UIView alloc] init];
+    self.view.backgroundColor = UIColor.brownColor;
+    
+    CGRect screenRect = UIScreen.mainScreen.bounds;
+    CGRect accessoryRect;
+    CGRect contentRect;
+    CGRectDivide(screenRect, &accessoryRect, &contentRect, 100, CGRectMinYEdge);
+    
+    UIView *accessoryView = [[UIView alloc] initWithFrame:accessoryRect];
+    self.contentView = [[UIView alloc] initWithFrame:contentRect];
+    self.contentView.clipsToBounds = YES;
+    
+    [self.view addSubview:accessoryView];
+    [self.view addSubview:self.contentView];
+    
+    accessoryView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.6];
+    self.contentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.4];
+    
+    // Controls
+    
+    self.textField = [[UITextField alloc] initWithFrame:CGRectMake(15, 50, 240, 35)];
+    [accessoryView addSubview:self.textField];
+    self.textField.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField.placeholder = @"Enter a label";
+    
+    self.textField.delegate = self;
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [accessoryView addSubview:button];
+    [button setTitle:@"Add" forState:UIControlStateNormal];
+    [button sizeToFit];
+    button.frame = CGRectOffset(button.frame, 270, 50);
+    
+    [button addTarget:self action:@selector(addCell) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    // Cells
+    
+    CLNCoolViewCell *subview1 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(20, 60, 200, 40)];
+    CLNCoolViewCell *subview2 = [[CLNCoolViewCell alloc] initWithFrame:CGRectMake(50, 120, 200, 40)];
+    
+    subview1.text = @"Hello World! 🌍🌎🌏🪐";
+    subview2.text = @"Cool View Cells Rock! 🎉🍾";
+    
+    [self.contentView addSubview:subview1];
+    [self.contentView addSubview:subview2];
+    
+    subview1.backgroundColor = UIColor.systemPurpleColor;
+    subview2.backgroundColor = UIColor.orangeColor;
+}
+
+@end
---
+++ Coolness2/Coolness2/CLNCoolViewCell.m
@@ -0,0 +1,128 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "CLNCoolViewCell.h"
+
+const UIEdgeInsets CLNTextInsets = {
+    .top = 7,
+    .bottom = 8,
+    .left = 12,
+    .right = 12
+};
+
+const CGPoint CLNTextOrigin = { 12, 7 };
+
+@interface CLNCoolViewCell ()
+@property (assign, nonatomic) BOOL highlighted;
+@property (class, readonly, nonatomic) NSDictionary *textAttributes;
+@end
+
+@implementation CLNCoolViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (!(self = [super initWithFrame:frame])) return nil;
+    
+    [self configureLayer];
+    [self configureGestureRecognizers];
+    
+    return self;
+}
+
+- (void)configureGestureRecognizers {
+    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(bounce)];
+    recognizer.numberOfTapsRequired = 2;
+    [self addGestureRecognizer:recognizer];
+}
+
+- (void)configureLayer {
+    self.layer.borderWidth = 3;
+    self.layer.borderColor = UIColor.whiteColor.CGColor;
+    self.layer.cornerRadius = 10;
+    
+    self.layer.masksToBounds = YES;
+}
+
+
++ (NSDictionary *)textAttributes {
+    static NSDictionary *attributes;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        attributes = @{ NSFontAttributeName: [UIFont boldSystemFontOfSize:20],
+                        NSForegroundColorAttributeName: UIColor.whiteColor };
+    });
+    return attributes;
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+    _highlighted = highlighted;
+    self.alpha = highlighted ? 0.5 : 1.0;
+}
+
+// MARK: - Animation
+
+- (void)bounce {
+    NSLog(@"In %s", __func__);
+    [self animateBounceWithDuration:1 size:CGSizeMake(120, 240)];
+}
+
+- (void)configureAnimationWithSize:(CGSize)size {
+    [UIView setAnimationRepeatCount:5];
+    [UIView setAnimationRepeatAutoreverses:YES];
+    CGAffineTransform translation = CGAffineTransformMakeTranslation(size.width, size.height);
+    self.transform = CGAffineTransformRotate(translation, M_PI_2);
+}
+
+- (void)animateBounceWithDuration:(NSTimeInterval)duration size:(CGSize)size {
+    typeof(self) __weak weakSelf = self;
+    [UIView animateWithDuration:duration
+                     animations:^{ [weakSelf configureAnimationWithSize:size]; }
+                     completion:^(BOOL finished) { weakSelf.transform = CGAffineTransformIdentity; }];
+}
+
+
+// MARK: - Drawing and resizing
+
+- (void)setText:(NSString *)text {
+    _text = [text copy];
+    [self sizeToFit];
+}
+
+- (CGSize)sizeThatFits:(CGSize)size {
+    CGSize newSize = [self.text sizeWithAttributes:self.class.textAttributes];
+    newSize.width += CLNTextInsets.left + CLNTextInsets.right;
+    newSize.height += CLNTextInsets.top + CLNTextInsets.bottom;
+    return newSize;
+}
+
+- (void)drawRect:(CGRect)rect {
+    [self.text drawAtPoint:CLNTextOrigin withAttributes:self.class.textAttributes];
+}
+
+
+// MARK: - UIResponder methods
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.superview bringSubviewToFront:self];
+    self.highlighted = YES;
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = touches.anyObject;
+    CGPoint currLocation = [touch locationInView:nil];
+    CGPoint prevLocation = [touch previousLocationInView:nil];
+    
+    CGFloat dx = currLocation.x - prevLocation.x;
+    CGFloat dy = currLocation.y - prevLocation.y;
+    
+    self.frame = CGRectOffset(self.frame, dx, dy);
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.highlighted = NO;
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.highlighted = NO;
+}
+
+@end
